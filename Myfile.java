2d1c469ff6bac4be4377ed32a03dfca4bdb21f62
Shreetik Sahani <shreetikkumar@gmail.com>
2022-02-21 23:54:57 +0530

    new method call in method branch

--- Myfile.java
+++ Myfile.java
@@ -4,7 +4,7 @@ public class Myfile {
     String name;
     int id;
 
-    Myfile setData(String name, int id) {
+    public Myfile setData(String name, int id) {
 
         Myfile obj = new Myfile();
         obj.name = name;
--- demo.java
+++ demo.java
@@ -1,8 +1,15 @@
 package gitTesting;
 
+import gitTesting.Myfile;
+
 class demo {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("second line");
+
+        Myfile my = new Myfile();
+
+        // my.setData("shreetik", 55);
+
     }
 }
